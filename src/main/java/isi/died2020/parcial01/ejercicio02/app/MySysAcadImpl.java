eff518b40f93a911af8921ab742b23711be57c59
AgusBertolin <agustinabertolin@gmail.com>
2020-05-21 22:20:30 -0300

    ejercicio02

--- src/main/java/isi/died2020/parcial01/ejercicio02/app/MySysAcadImpl.java
+++ src/main/java/isi/died2020/parcial01/ejercicio02/app/MySysAcadImpl.java
@@ -2,10 +2,12 @@ package isi.died2020.parcial01.ejercicio02.app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import isi.died2020.parcial01.ejercicio02.db.BaseDeDatos;
 import isi.died2020.parcial01.ejercicio02.db.BaseDeDatosExcepcion;
 import isi.died2020.parcial01.ejercicio02.dominio.*;
+import isi.died2020.parcial01.ejercicio02.dominio.Inscripcion.Estado;
 
 
 public class MySysAcadImpl implements MySysAcad {
@@ -13,6 +15,14 @@ public class MySysAcadImpl implements MySysAcad {
 
 
 	private List<Materia> materia = new ArrayList<Materia>();
+	
+	private class MyException extends Exception {
+
+	    public MyException(String msg) {
+	        super(msg);
+	    }
+	    
+	}
 	
 	@Override
 	public void registrarMateria(Materia d) {
@@ -37,16 +47,21 @@ public class MySysAcadImpl implements MySysAcad {
 	@Override
 	public void inscribirAlumnoCursada(Docente d, Alumno a, Materia m, Integer cicloLectivo) {
 		Inscripcion insc = new Inscripcion(cicloLectivo,Inscripcion.Estado.CURSANDO);
-		d.agregarInscripcion(insc);
-		a.addCursada(insc);
-		m.addInscripcion(insc);
 		// DESCOMENTAR Y gestionar excepcion
 		
 		try {
-			DB.guardar(insc);
+			List<Inscripcion> aux = a.getMaterias().stream().filter(x -> x.getMateria().equals(m)).collect(Collectors.toList());
+			if(!aux.isEmpty() && aux.get(0).getEstado() != Estado.LIBRE) throw new MyException("Materia en estado distinto de libre");
+			else {
+				d.agregarInscripcion(insc);
+				a.addCursada(insc);
+				m.addInscripcion(insc);
+				DB.guardar(insc);
+			}
 		} catch (BaseDeDatosExcepcion e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (MyException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
